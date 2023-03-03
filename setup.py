aff0858092877d906b75f767f62b3d881b49ef45
Johannes Dorn <johannes@dorn.tk>
2023-03-03 13:14:01 +0100

    updated setup.py to make sure the arviz API is right

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 setuptools.setup(
     name="bayesify",  # Replace with your own username
-    version="0.1",
-    python_requires=">=3.9, <3.10",
+    version="0.1.1",
+    python_requires=">=3.7, <3.11",
     author="Johannes Dorn",
     author_email="johannes.dorn@uni-leipzig.de",
     description="Uncertainty-aware NFP Predictions with Probabilistic Programming",
@@ -13,7 +13,7 @@ setuptools.setup(
         "numpy~=1.23",
         "matplotlib~=3.5.1",
         "pandas~=1.5",
-        "arviz~=0.12",
+        "arviz==0.12.1",
         "seaborn~=0.11.2",
         "scipy~=1.8.0",
         "scikit-learn~=1.1.0rc1",
